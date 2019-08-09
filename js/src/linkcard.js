2edca6d530029beeb074aaded341b98e967bd7d9
912011727@qq.com <912011727@qq.com>
2019-08-09 20:25:21 +0800

    Site updated: 2019-08-09 20:25:15

--- js/src/linkcard.js
+++ js/src/linkcard.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>window.onload=function(){
+window.onload=function(){
     var LinkCards=document.getElementsByClassName('LinkCard');
     if(LinkCards.length != 0){
     var LinkCard=LinkCards[0];
