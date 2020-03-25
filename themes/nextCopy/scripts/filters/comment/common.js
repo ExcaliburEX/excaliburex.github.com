748e975325e887820bedf44d105ba74e008a3ff1
912011727@qq.com <912011727@qq.com>
2020-03-26 01:21:19 +0800

    📦 2020年3月26日01:21:01 next的copy版本

---
+++ themes/nextCopy/scripts/filters/comment/common.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  iconText: `
+    <span class="post-meta-item-icon">
+      <i class="fa fa-comment-o"></i>
+    </span>
+    <span class="post-meta-item-text">{{ __('post.comments_count') + __('symbol.colon') }}</span>
+  `
+};
